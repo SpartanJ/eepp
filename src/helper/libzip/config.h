99f14bbd62706f7624316e23dfd9015e6545add5
spartanj <none@none>
2010-08-23 15:01:02 -0300

    Fixed Windows compilation.

--- src/helper/libzip/config.h
+++ src/helper/libzip/config.h
@@ -8,11 +8,20 @@
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
+#if !defined( __WIN32__ ) && !defined( _WIN32 )
 /* Define to 1 if you have the `fseeko' function. */
 #define HAVE_FSEEKO 1
 
 /* Define to 1 if you have the `ftello' function. */
 #define HAVE_FTELLO 1
+
+/* Define to 1 if `tm_zone' is a member of `struct tm'. */
+#define HAVE_STRUCT_TM_TM_ZONE 1
+
+/* Define to 1 if you have the `mkstemp' function. */
+#define HAVE_MKSTEMP 1
+
+#endif
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
@@ -22,9 +31,6 @@
 
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the `mkstemp' function. */
-#define HAVE_MKSTEMP 1
 
 /* Define to 1 if you have the `MoveFileExA' function. */
 /* #undef HAVE_MOVEFILEEXA */
@@ -40,9 +46,6 @@
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
-
-/* Define to 1 if `tm_zone' is a member of `struct tm'. */
-#define HAVE_STRUCT_TM_TM_ZONE 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
