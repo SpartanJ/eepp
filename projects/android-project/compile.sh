8a58b6c7deaefe6f5e9990304504be70846ad7bf
Martín Lucas Golini <spartanj@gmail.com>
2013-07-14 23:27:42 -0300

    Now android package name can be changed without breaking the app.

--- projects/android-project/compile.sh
+++ projects/android-project/compile.sh
@@ -5,5 +5,5 @@ ndk-build NDK_LOG=1 NDK_DEBUG=1 -j4
 rm -rf ./bin
 rm -rf ./gen
 ant debug
-adb install -r bin/EEPPApp-debug.apk
+adb install -r bin/eepp-app-debug.apk
 ndk-gdb --force --start --verbose
