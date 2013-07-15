9402c8a34e63c039917575ba0eed4fc714540722
Martín Lucas Golini <spartanj@gmail.com>
2013-07-14 22:13:12 -0300

    Ups, broke android build, now it's working again.

--- projects/android-project/compile.sh
+++ projects/android-project/compile.sh
@@ -3,6 +3,7 @@ cd $(dirname "$0")
 export NDK_PROJECT_PATH=$(dirname "$0")
 ndk-build NDK_LOG=1 NDK_DEBUG=1 -j4
 rm -rf ./bin
+rm -rf ./gen
 ant debug
 adb install -r bin/EEPPApp-debug.apk
 ndk-gdb --force --start --verbose
