f276feb2742b115f24961664a7377e3db73b73ab
Martín Lucas Golini <spartanj@gmail.com>
2013-11-13 02:35:56 -0300

    Updated iOS libSDL2.a precompiled library. Updated SOIL2 ( it seems that i forgot to do it ).

--- projects/ios/cross-install.sh
+++ projects/ios/cross-install.sh
@@ -2,4 +2,4 @@
 ln -sf ../../../assets ./eepp.app/
 echo "The fist parameter must be the path of the binary file to install. Example: ../../eetest-debug"
 cp $1 ./eepp.app/eepp
-make install
+make install $2
