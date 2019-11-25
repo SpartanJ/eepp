634ea7f3cb02784e7fba57dc7d97aa6c82ef8c61
Martín Lucas Golini <spartanj@gmail.com>
2019-11-24 22:12:19 -0300

    Fixed frambuffer size.
    --HG--
    branch : dev

--- src/eepp/scene/scenenode.cpp
+++ src/eepp/scene/scenenode.cpp
@@ -247,7 +247,8 @@ void SceneNode::drawFrameBuffer() {
 		if ( mFrameBuffer->hasColorBuffer() ) {
 			mFrameBuffer->draw( Rect( 0, 0, mSize.getWidth(), mSize.getHeight() ), Rect( mScreenPos.x, mScreenPos.y, mScreenPos.x + mSize.getWidth(), mScreenPos.y + mSize.getHeight() ) );
 		} else {
-			TextureRegion textureRegion( mFrameBuffer->getTexture()->getTextureId(), Rect( 0, 0, mSize.getWidth(), mSize.getHeight() ) );
+			Rect r = Rect( 0, 0, mSize.getWidth(), mSize.getHeight() );
+			TextureRegion textureRegion( mFrameBuffer->getTexture()->getTextureId(), r, r.getSize().asFloat() );
 			textureRegion.draw( mScreenPosi.x, mScreenPosi.y, Color::White, getRotation(), getScale() );
 		}
 	}
