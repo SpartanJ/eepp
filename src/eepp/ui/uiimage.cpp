f015284f86d4f084862716cd65b9a2b1f1e4e7f2
Martín Lucas Golini <spartanj@gmail.com>
2019-11-29 01:50:49 -0300

    UIImage size fix.
    --HG--
    branch : dev-css-refactor

--- src/eepp/ui/uiimage.cpp
+++ src/eepp/ui/uiimage.cpp
@@ -111,7 +111,7 @@ void UIImage::calcDestSize() {
 		if ( NULL == mDrawable)
 			return;
 
-		mDestSize = PixelDensity::dpToPx( mDrawable->getSize() );
+		mDestSize = mDrawable->getSize();
 	}
 
 	mDestSize = mDestSize.floor();
