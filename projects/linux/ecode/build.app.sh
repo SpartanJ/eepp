b6ac472d732f14cf38553fd4e462169e074b4b34
Martín Lucas Golini <spartanj@gmail.com>
2022-06-07 20:13:22 -0300

    ecode: Improved Linux build package script.

--- projects/linux/ecode/build.app.sh
+++ projects/linux/ecode/build.app.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
-CANONPATH=`readlink -f "$0"`
-DIRPATH="`dirname "$CANONPATH"`"
-cd $DIRPATH
-cd ../../../
+#!/bin/bash
+CANONPATH=$(readlink -f "$0")
+DIRPATH="$(dirname "$CANONPATH")"
+cd "$DIRPATH" || exit
+cd ../../../ || exit
 premake4 --with-mojoal gmake
-cd make/linux
-make -j$(nproc) config=release ecode
-cd $DIRPATH
+cd make/linux || exit
+make -j"$(nproc)" config=release ecode
+cd "$DIRPATH" || exit
 rm -rf ./ecode.app
 mkdir -p ecode.app/assets
 mkdir -p ecode.app/libs
@@ -18,11 +18,9 @@ cp ../../../libs/linux/libeepp.so ecode.app/libs/
 cp ../../../bin/ecode ecode.app/
 cp -L $(whereis libSDL2-2.0.so.0 | awk '{print $NF}') ecode.app/libs/
 strip ecode.app/libs/libSDL2-2.0.so.0
-#cp -r ../../../bin/assets ecode.app/assets
 mkdir -p ecode.app/assets/colorschemes
 mkdir -p ecode.app/assets/fonts
 cp -r ../../../bin/assets/colorschemes ecode.app/assets/
-#cp -r ../../../bin/assets/fonts ecode.app/assets/
 cp -r ../../../bin/assets/fonts/DejaVuSansMono.ttf ecode.app/assets/fonts/
 cp -r ../../../bin/assets/fonts/nonicons.ttf ecode.app/assets/fonts/
 cp -r ../../../bin/assets/fonts/NotoSans-Regular.ttf ecode.app/assets/fonts/
@@ -33,4 +31,14 @@ cp -r ../../../bin/assets/plugins ecode.app/assets/
 cp -r ../../../bin/assets/icon ecode.app/assets/
 mkdir ecode.app/assets/ui
 cp ../../../bin/assets/ui/breeze.css ecode.app/assets/ui/
-appimagetool ecode.app
+
+export APPIMAGETOOL="appimagetool"
+
+if ! command -v appimagetool &> /dev/null
+then
+	wget -nc "https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-$(arch).AppImage"
+	APPIMAGETOOL="./appimagetool-$(arch).AppImage"
+	chmod +x "$APPIMAGETOOL"
+fi
+
+$APPIMAGETOOL ecode.app
