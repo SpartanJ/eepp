2fb0c77da7d2f22de022d0b9e46590ffb76b9507
Martín Lucas Golini <spartanj@gmail.com>
2023-02-14 10:58:55 -0300

    Pump version.

--- include/eepp/version.hpp
+++ include/eepp/version.hpp
@@ -6,7 +6,7 @@
 
 #define EEPP_MAJOR_VERSION 2
 #define EEPP_MINOR_VERSION 5
-#define EEPP_PATCH_LEVEL 6
+#define EEPP_PATCH_LEVEL 7
 #define EEPP_CODENAME "Bindu"
 
 /** The compiled version of the library */
