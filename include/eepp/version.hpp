b1b0a228090e0be8c3bfeb657cd8b1c7f8342849
Martín Lucas Golini <spartanj@gmail.com>
2023-03-27 14:53:44 -0300

    Pump version.

--- include/eepp/version.hpp
+++ include/eepp/version.hpp
@@ -5,9 +5,9 @@
 #include <string>
 
 #define EEPP_MAJOR_VERSION 2
-#define EEPP_MINOR_VERSION 5
-#define EEPP_PATCH_LEVEL 9
-#define EEPP_CODENAME "Bindu"
+#define EEPP_MINOR_VERSION 6
+#define EEPP_PATCH_LEVEL 0
+#define EEPP_CODENAME "Anathapindika"
 
 /** The compiled version of the library */
 #define EEPP_VERSION( x )                  \
