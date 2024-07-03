81d04448ec45fd251b41cca9e6be69dfc569237b
Martín Lucas Golini <spartanj@gmail.com>
2024-07-03 01:15:17 -0300

    Fix scale in tab close button.

--- src/tools/ecode/applayout.xml.hpp
+++ src/tools/ecode/applayout.xml.hpp
@@ -423,26 +423,26 @@ Anchor.error:hover {
 	opacity: 1;
 }
 #code_container Tab > Tab::close {
-	foreground-image: icon(tab-close, 15dp);
+	foreground-image: icon(tab-close, 10dp);
 	foreground-tint: var(--tab-close);
+	foreground-size: 10dp;
 	foreground-position: center;
-	foreground-size: auto;
 }
 #code_container Tab > Tab::close:hover {
 	foreground-tint: var(--tab-close-hover);
 }
 #code_container Tab.tab_modified > tab::close {
-	foreground-image: icon(circle-filled, 16dp);
+	foreground-image: icon(circle-filled, 6dp);
 	foreground-tint: var(--primary);
-	foreground-size: auto;
+	foreground-size: 6dp;
 	foreground-position: center;
 	opacity: 1;
 }
 #code_container Tab.tab_modified > Tab::close:hover {
-	foreground-image: icon(tab-close, 15dp);
+	foreground-image: icon(tab-close, 10dp);
 	foreground-tint: var(--tab-close-hover);
+	foreground-size: 10dp;
 	foreground-position: center;
-	foreground-size: auto;
 }
 #project_view ScrollBar {
 	opacity: 0;
