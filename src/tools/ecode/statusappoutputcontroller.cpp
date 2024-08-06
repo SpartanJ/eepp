733ea08362c303d5441cdc3e4406d4124260c89f
Martín Lucas Golini <spartanj@gmail.com>
2024-08-06 00:21:28 -0300

    Nit.

--- src/tools/ecode/statusappoutputcontroller.cpp
+++ src/tools/ecode/statusappoutputcontroller.cpp
@@ -82,10 +82,10 @@ void StatusAppOutputController::run( const ProjectBuildCommand& runData,
 	mStopButton->setEnabled( true );
 
 	const auto updateRunButton = [this]() {
-		UIPushButton* buildButton = getRunButton( mApp );
-		if ( buildButton ) {
-			buildButton->runOnMainThread(
-				[this, buildButton] { buildButton->setText( mApp->i18n( "run", "Run" ) ); } );
+		UIPushButton* runButton = getRunButton( mApp );
+		if ( runButton ) {
+			runButton->runOnMainThread(
+				[this, runButton] { runButton->setText( mApp->i18n( "run", "Run" ) ); } );
 		}
 		mRunButton->setEnabled( true );
 		mStopButton->setEnabled( false );
