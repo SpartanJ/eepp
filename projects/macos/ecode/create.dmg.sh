23bde61361eb42f9c670d9dbbf1b840710055c16
Martín Lucas Golini <spartanj@gmail.com>
2024-09-14 12:46:29 -0300

    Don't delete different arch dmg when rebuilding.

--- projects/macos/ecode/create.dmg.sh
+++ projects/macos/ecode/create.dmg.sh
@@ -6,5 +6,5 @@ ECODE_MAJOR_VERSION=$(grep "define ECODE_MAJOR_VERSION" $VERSIONPATH | awk '{pri
 ECODE_MINOR_VERSION=$(grep "define ECODE_MINOR_VERSION" $VERSIONPATH | awk '{print $3}')
 ECODE_PATCH_LEVEL=$(grep "define ECODE_PATCH_LEVEL" $VERSIONPATH | awk '{print $3}')
 
-rm -f ./*.dmg
+rm -f ./*arm64.dmg
 create-dmg --icon ecode 0 16 --window-size 256 256 --app-drop-link 0 80 --icon-size 32 --text-size 12 --volicon ../../../bin/assets/icon/ecode.icns ecode-macos-"$ECODE_MAJOR_VERSION"."$ECODE_MINOR_VERSION"."$ECODE_PATCH_LEVEL"-"$(uname -m)".dmg ecode.app
