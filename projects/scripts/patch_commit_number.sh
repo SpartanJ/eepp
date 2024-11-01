71a38e81beb399aa31536dfd11ee4c8db656a56b
Martín Lucas Golini <spartanj@gmail.com>
2024-11-01 11:13:34 -0300

    Fix patch commit number replacement in macOS and FreeBSD.

--- projects/scripts/patch_commit_number.sh
+++ projects/scripts/patch_commit_number.sh
@@ -7,7 +7,7 @@ COMMIT_NUMBER=$(git rev-list "$(git tag --sort=-creatordate | grep ecode | sed -
 FILE_PATH="../../src/tools/ecode/version.hpp"
 
 if [[ "$OSTYPE" == "darwin"* || "$OSTYPE" == "freebsd"* ]]; then
-    sed -i '' "s/#define ECODE_COMMIT_NUMBER [0-9]\+/#define ECODE_COMMIT_NUMBER $COMMIT_NUMBER/" "$FILE_PATH"
+    perl -i -pe "s/#define ECODE_COMMIT_NUMBER [0-9]+/#define ECODE_COMMIT_NUMBER $COMMIT_NUMBER/" "$FILE_PATH"
 else
     sed -i "s/#define ECODE_COMMIT_NUMBER [0-9]\+/#define ECODE_COMMIT_NUMBER $COMMIT_NUMBER/" "$FILE_PATH"
 fi
