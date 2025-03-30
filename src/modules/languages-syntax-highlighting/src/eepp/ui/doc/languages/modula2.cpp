82793f38bebfd2059cb1f4b49ae67be879ac8742
Curculigo <giahung1997@proton.me>
2025-03-30 22:36:11 +0700

    modula2

---
+++ src/modules/languages-syntax-highlighting/src/eepp/ui/doc/languages/modula2.cpp
@@ -0,0 +1,103 @@
+#include <eepp/ui/doc/languages/modula2.hpp>
+#include <eepp/ui/doc/syntaxdefinitionmanager.hpp>
+
+namespace EE { namespace UI { namespace Doc { namespace Language {
+
+void addModula2() {
+
+	auto& sd = SyntaxDefinitionManager::instance()->add(
+
+		{ "Ada",
+		  { "%.def$", "%.mod$" },
+		  {
+			  { { "\"", "\"", "\\" }, "string" },
+			  { { "%(%*", "%*%)" }, "comment" },
+			  { { "[%:%;%=%<%>%&%+%-%*%/%.%(%)]" }, "operator" },
+			  { { "-?%d+[%d%.eE_]*" }, "number" },
+			  { { "[%a_][%w_]*%f[(]" }, "function" },
+			  { { "[%a_][%w_]*" }, "normal" },
+
+		  },
+		  {
+			  { "ARRAY", "keyword" },
+			  { "BEGIN", "keyword" },
+			  { "BY", "keyword" },
+			  { "CASE", "keyword" },
+			  { "CONST", "keyword" },
+			  { "DEFINITION", "keyword" },
+			  { "DO", "keyword" },
+			  { "ELSE", "keyword" },
+			  { "ELSIF", "keyword" },
+			  { "END", "keyword" },
+			  { "EXCEPT", "keyword" },
+			  { "EXIT", "keyword" },
+			  { "EXPORT", "keyword" },
+			  { "FINALLY", "keyword" },
+			  { "FOR", "keyword" },
+			  { "FORWARD", "keyword" },
+			  { "FROM", "keyword" },
+			  { "IF", "keyword" },
+			  { "IMPLEMENTATION", "keyword" },
+			  { "IMPORT", "keyword" },
+			  { "IN", "keyword" },
+			  { "LOOP", "keyword" },
+			  { "MODULE", "keyword" },
+			  { "OF", "keyword" },
+			  { "PACKEDSET", "keyword" },
+			  { "POINTER", "keyword" },
+			  { "PROCEDURE", "keyword" },
+			  { "QUALIFIED", "keyword" },
+			  { "RECORD", "keyword" },
+			  { "REPEAT", "keyword" },
+			  { "RETRY", "keyword" },
+			  { "RETURN", "keyword" },
+			  { "SET", "keyword" },
+			  { "THEN", "keyword" },
+			  { "TO", "keyword" },
+			  { "TYPE", "keyword" },
+			  { "UNTIL", "keyword" },
+			  { "VAR", "keyword" },
+			  { "WHILE", "keyword" },
+			  { "WITH", "keyword" },
+ 			  { "ABS", "function" },
+			  { "ADR", "function" },
+			  { "ASH", "function" },
+			  { "AND", "function" },
+			  { "CAP", "function" },
+			  { "DEC", "function" },
+			  { "DISPOSE", "function" },
+ 			  { "DIV", "function" },
+			  { "EXCL", "function" },
+			  { "FLOAT", "function" },
+			  { "INC", "function" },
+			  { "INCL", "function" },
+			  { "HALT", "function" },
+ 			  { "HIGH", "function" },
+			  { "NEW", "function" },
+			  { "MOD", "function" },
+			  { "NOT", "function" },
+			  { "ODD", "function" },
+			  { "OR", "function" },
+ 			  { "PROC", "function" },
+			  { "ROUND", "function" },
+			  { "SIZE", "function" },
+			  { "TSIZE", "function" },
+			  { "BITSET", "keyword2" },
+			  { "BOOLEAN", "keyword2" },
+ 			  { "CARDINAL", "keyword2" },
+			  { "CHAR", "keyword2" },
+			  { "INTEGER", "keyword2" },
+			  { "REAL", "keyword2" },
+			  { "TRUE", "literal" },
+			  { "FALSE", "literal" },
+			  { "NIL", "literal" }
+		  },
+		  "--",
+		  {}
+
+		} );
+
+	sd.setCaseInsensitive( true );
+}
+
+}}}} // namespace EE::UI::Doc::Language
---
+++ src/modules/languages-syntax-highlighting/src/eepp/ui/doc/languages/modula2.hpp
@@ -0,0 +1,10 @@
+#ifndef EE_UI_DOC_MODULA2
+#define EE_UI_DOC_MODULA2
+
+namespace EE { namespace UI { namespace Doc { namespace Language {
+
+extern void addModula2();
+
+}}}}
+
+#endif
