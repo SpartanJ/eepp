8351cee5b1e04764ba9d23de9b0180795183040c
Curculigo <giahung1997@proton.me>
2025-03-31 10:34:51 +0700

    "Ü" programming language

---
+++ src/modules/languages-syntax-highlighting/src/eepp/ui/doc/languages/u.hpp
@@ -0,0 +1,10 @@
+#ifndef EE_UI_DOC_U
+#define EE_UI_DOC_U
+
+namespace EE { namespace UI { namespace Doc { namespace Language {
+
+extern void addU();
+
+}}}} // namespace EE::UI::Doc::Language
+
+#endif
