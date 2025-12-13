27c83edbe38bf051247bac84cb75459a6bfe9964
blueberryscone238 <tngiahung1997@proton.me>
2025-12-13 18:35:33 +0700

    Update patch_commit_number.ps1

--- projects/scripts/patch_commit_number.ps1
+++ projects/scripts/patch_commit_number.ps1
@@ -1,15 +1,13 @@
-Set-Location $PSScriptRoot
+Set-Location (Resolve-Path "$PSScriptRoot\..\..")
 
 $COMMIT_NUMBER = git rev-list "$((git tag --sort=-creatordate | Select-String ecode | Select-Object -First 1).Line)..HEAD" --count
 
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 
-$FILE_PATH = "../../src/tools/ecode/version.hpp"
+$FILE_PATH = ".\src\tools\ecode\version.hpp"
 
 try {
   (Get-Content $FILE_PATH) -replace '#define ECODE_COMMIT_NUMBER \d+', "#define ECODE_COMMIT_NUMBER $COMMIT_NUMBER" | Set-Content $FILE_PATH
 } catch {
   exit 1
 }
-
-Set-Location (Resolve-Path "$PSScriptRoot\..\..")
