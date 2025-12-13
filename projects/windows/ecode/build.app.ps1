72bc325b625556e2ad4ca79bf0cc3b744aa04884
blueberryscone238 <tngiahung1997@proton.me>
2025-12-13 09:48:39 +0700

    Update build.app.ps1

--- projects/windows/ecode/build.app.ps1
+++ projects/windows/ecode/build.app.ps1
@@ -8,7 +8,6 @@ if ($arch -eq "arm64") {
   Invoke-WebRequest -Uri "https://github.com/premake/premake-core/releases/download/v5.0.0-beta6/premake-5.0.0-beta6-windows.zip" -OutFile "premake-5.0.0-beta6-windows.zip"
   Expand-Archive -LiteralPath "premake-5.0.0-beta6-windows.zip" -DestinationPath .
   .\premake5.exe --windows-vc-build --with-text-shaper --arch=arm64 --disable-static-build vs2022
-  .\projects\scripts\patch_commit_number.ps1
   & "$env:MSBUILD_PATH/MSBuild.exe" .\make\windows\eepp.sln -m /t:ecode /p:Platform=ARM64 /p:Configuration=release
   New-Item -Name "ecode" -ItemType Directory
   .\projects\scripts\copy_ecode_assets.ps1 .\bin .\ecode
@@ -20,7 +19,6 @@ else {
   Invoke-WebRequest -Uri "https://github.com/premake/premake-core/releases/download/v5.0.0-beta6/premake-5.0.0-beta6-windows.zip" -OutFile "premake-5.0.0-beta6-windows.zip"
   Expand-Archive -LiteralPath "premake-5.0.0-beta6-windows.zip" -DestinationPath .
   .\premake5.exe --windows-vc-build --with-text-shaper --disable-static-build vs2022
-  .\projects\scripts\patch_commit_number.ps1
   & "$env:MSBUILD_PATH/MSBuild.exe" .\make\windows\eepp.sln -m /t:ecode /p:Platform=x64 /p:Configuration=release
   New-Item -Name "ecode" -ItemType Directory
   .\projects\scripts\copy_ecode_assets.ps1 .\bin .\ecode
